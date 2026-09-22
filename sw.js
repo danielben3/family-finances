014b0eb121efdf081442f8ed0ce585c410d3d330
danielben3 <danielben3@users.noreply.github.com>
2026-09-22 20:39:49 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Family Finances PWA
-const CACHE_NAME = 'family-finances-v6';
+const CACHE_NAME = 'family-finances-v7';
 const PRECACHE_URLS = [
   '/family-finances/',
   '/family-finances/index.html',
